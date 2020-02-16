66c0d7abf320bcb411dfd09c1f23c9f0b985d011
elizabethlin <elin6@uw.edu>
2020-02-15 20:00:57 -0800

    y

---
+++ sfd.py
@@ -0,0 +1,11 @@
+print ("15 / 4 is:" + str(15 / 4))        # Difference from Python 2!
+print ("15.0 / 4 is:", 15.0 / 4)
+print ("15 / 4.0 is:", 15 / 4.0)
+
+# Type conversion examples:
+print ("float(15) is:", float(15))
+print ("int(15.0) is:", int(15.0))
+print ("int(15.5) is:", int(15.5))
+print ("int(\"15\") is:", int("15"))
+print ("str(15.5) is:", str(15.5))
+print ("float(15) / 4 is:", float(15) / 4)
